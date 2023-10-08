b423978875eb879c5ac2d9d5445ebe5c044a6448
Rahul Butani <rrbutani@users.noreply.github.com>
2023-10-07 23:48:22 -0700

    ci: use `bashInteractive` in the shell

--- test/shell.nix
+++ test/shell.nix
@@ -1,6 +1,6 @@
 { np ? import <nixpkgs> {}, includeHello ? true, includeTiny ? false }: np.mkShell {
   name = "test-shell";
-  packages = with np; ([ coreutils gnugrep diffutils python3 ]
+  packages = with np; ([ coreutils bashInteractive gnugrep diffutils python3 ]
     ++ np.lib.optional includeHello hello
     ++ np.lib.optional includeTiny tiny
   );
